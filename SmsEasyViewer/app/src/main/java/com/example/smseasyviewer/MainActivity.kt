90936f70a3fb5f7b70ef97b8b2306c2a5c792809
Ron <lavitr01051977@gmail.com>
2022-01-23 03:20:27 +0300

    feat: implemented read inbox logic

---
+++ SmsEasyViewer/app/src/main/java/com/example/smseasyviewer/MainActivity.kt
@@ -0,0 +1,83 @@
+package com.example.smseasyviewer
+
+import android.content.pm.PackageManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.Telephony
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
+
+class MainActivity : AppCompatActivity() {
+    private val requestPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted: Boolean ->
+            if (isGranted) {
+                Log.d("after", "!! After permission Granted!!!")
+            } else {
+                Log.d("after", "After not!1 permission Granted")
+
+            }
+        }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val textView = findViewById<TextView>(R.id.text)
+        val textView2 = findViewById<TextView>(R.id.text2)
+
+        textView.text = "Huj them all!!"
+
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+            readSms()
+        }
+
+        when (PackageManager.PERMISSION_GRANTED) {
+            ContextCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.READ_SMS
+            ) -> {
+                Log.d("after", "After permission Granted444")
+            }
+            else -> {
+                requestPermissionLauncher.launch(
+                    android.Manifest.permission.READ_SMS
+                )
+            }
+        }
+    }
+
+    private fun readSms() {
+        val numberCol = Telephony.TextBasedSmsColumns.ADDRESS
+        val textCol = Telephony.TextBasedSmsColumns.BODY
+        val typeCol = Telephony.TextBasedSmsColumns.TYPE // 1 - Inbox, 2 - Sent
+
+        val projection = arrayOf(numberCol, textCol, typeCol)
+
+        val cursor = contentResolver.query(
+            Telephony.Sms.CONTENT_URI,
+            projection, null, null, null
+        )
+
+        val numberColIdx = cursor!!.getColumnIndex(numberCol)
+        val textColIdx = cursor.getColumnIndex(textCol)
+        val typeColIdx = cursor.getColumnIndex(typeCol)
+        var i = 0
+
+        while (cursor.moveToNext() && i<15) {
+            val number = cursor.getString(numberColIdx)
+            val text = cursor.getString(textColIdx)
+            val type = cursor.getString(typeColIdx)
+
+            Log.d("MY_APP::$i", "$number :: $text :: $type")
+            i++
+        }
+
+        cursor.close()
+    }
+}
