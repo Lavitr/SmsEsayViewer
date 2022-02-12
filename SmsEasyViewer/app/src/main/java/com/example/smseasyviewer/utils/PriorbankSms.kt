7270e03ec1e6731b5c190d049cda2f86ccf26cec
Ron <lavitr01051977@gmail.com>
2022-02-12 20:33:18 +0300

    feat: console removed

--- SmsEasyViewer/app/src/main/java/com/example/smseasyviewer/utils/PriorbankSms.kt
+++ SmsEasyViewer/app/src/main/java/com/example/smseasyviewer/utils/PriorbankSms.kt
@@ -9,17 +9,10 @@ fun getPriorData(textBody: String, dataSet: HashMap<String, Int>) {
         val twoStrings = textBody.trim().split("BYN. BLR")
         if (twoStrings.size == 2) {
             val location = twoStrings[1].split("Dostupno")
-            if (textBody.trim().contains("WWW", true)) {
-                Log.d("PATTERN", "$twoStrings")
-            }
             val sum = paymentPattern.find(twoStrings[0])
             val value = sum?.value.toString().replace("Oplata", "").trim()
             val intValue = value.split(".")[0].toInt()
             val locationName = ConvertLocationName(location[0])
-
-            if (locationName.trim().contains("WWW", true)) {
-                Log.d("PATTERN", "$textBody")
-            }
             if (intValue >= 1) {
                 if (dataSet.containsKey(locationName)) {
                     dataSet[locationName] = dataSet[locationName]!! + intValue
